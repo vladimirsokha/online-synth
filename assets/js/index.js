eead3fa844efa2caae3933fdedacbcc12624111b
Explodeus <volodya.sokha@gmail.com>
2020-08-29 20:40:55 +0300

    Sound buttons and sounds added

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,46 @@
-console.log("HELLO MOTHERFUCKING WORLD!!");
+var context = new AudioContext();
+var o = context.createOscillator();
+o.type = "sine";
 
 function fuckingNoise() {
-    var context = new AudioContext();
-    var o = context.createOscillator();
-    o.type = "sine";
+    o.connect(context.destination);
+    o.start();
+}
+
+var frequencys = ['261.6', '293.7', '329.6', '349.2', '392.0', '440.0', '493.9'];
+
+function playC4(){
+    o.frequency.value = frequencys[0];
+    o.connect(context.destination);
+    o.start();
+}
+function playD4(){
+    o.frequency.value = frequencys[1];
+    o.connect(context.destination);
+    o.start();
+}
+function playE4(){
+    o.frequency.value = frequencys[2];
+    o.connect(context.destination);
+    o.start();
+}
+function playF4(){
+    o.frequency.value = frequencys[3];
+    o.connect(context.destination);
+    o.start();
+}
+function playG4(){
+    o.frequency.value = frequencys[4];
+    o.connect(context.destination);
+    o.start();
+}
+function playA4(){
+    o.frequency.value = frequencys[5];
+    o.connect(context.destination);
+    o.start();
+}
+function playB4(){
+    o.frequency.value = frequencys[6];
     o.connect(context.destination);
     o.start();
 }
