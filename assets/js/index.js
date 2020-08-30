273f411390021c7f586bc7d04a1f869e8229bfc8
Explodeus <volodya.sokha@gmail.com>
2020-08-30 11:54:40 +0300

    Added removed item

--- assets/js/index.js
+++ assets/js/index.js
@@ -49,6 +49,9 @@ function beep(frequency) {
 }
 
 window.addEventListener("keydown", function (event) {
+    if (event.defaultPrevented) {
+        return; // Do nothing if the event was already processed
+    }
 
     switch (event.key) {
         case "z":
